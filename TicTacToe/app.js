21641bfd5108ea79bcc3c304ff1c206fd996a5dd
Swati Pal <swatipal39@gmail.com>
2024-11-02 19:14:24 +0530

    Added condition for identifying draw in a match

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -4,75 +4,87 @@ let newGameBtn = document.querySelector("#newGame-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-
 let turn0 = true; //playerX, player0
+let count = 0;
 
 const winPatterns = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
 ];
 
-const resetGame = ()=>{
+// Function to reset the game
+const resetGame = () => {
     turn0 = true;
+    count = 0;
     enableBtns();
     msgContainer.classList.add("hide");
-}
+};
 
-boxes.forEach((box)=>{
-    box.addEventListener("click",()=>{
-        console.log("box was clicked");
-        if(turn0){
-            box.innerText="0";
-            turn0=false;
-        }else{
-            box.innerText="X";
-            turn0=true;
+// Setting up click events for each box
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turn0) {
+            box.innerText = "0";
+            turn0 = false;
+        } else {
+            box.innerText = "X";
+            turn0 = true;
         }
-        box.disabled=true;
-
+        count++;
+        box.disabled = true;
         checkWinner();
     });
 });
 
-const disableBtns = ()=>{
-    for(let box of boxes){
-        box.disabled=true;
-    }
-}
+// Function to disable buttons
+const disableBtns = () => {
+    boxes.forEach((box) => (box.disabled = true));
+};
 
-const enableBtns = ()=>{
-    for(let box of boxes){
-        box.disabled=false;
-        box.innerText="";
-    }
-}
+// Function to enable buttons
+const enableBtns = () => {
+    boxes.forEach((box) => {
+        box.disabled = false;
+        box.innerText = "";
+    });
+};
 
-const showWinner= (winner)=>{
-    msg.innerText = `Congratulations, Winner is ${winner}`;
+// Function to display winner message
+const showWinner = (winner) => {
+    if (winner === "Draw") {
+        msg.innerText = "It's a Draw!";
+    } else {
+        msg.innerText = `Congratulations, Winner is ${winner}`;
+    }
     msgContainer.classList.remove("hide");
     disableBtns();
-}
+};
 
-const checkWinner = ()=>{
-    for( let pattern of winPatterns){
-        
+// Function to check for a winner
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
 
-        if(pos1Val !="" && pos2Val != "" && pos3Val != ""){
-            if(pos1Val === pos2Val && pos2Val === pos3Val){
-                showWinner(pos1Val);
-            }
+        if (pos1Val !== "" && pos1Val === pos2Val && pos2Val === pos3Val) {
+            showWinner(pos1Val);
+            return;
         }
+    }
+
+    // Check for a draw if all boxes are filled
+    if (count === 9) {
+        showWinner("Draw");
     }
 };
 
+// Adding event listeners for reset and new game buttons
 newGameBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
